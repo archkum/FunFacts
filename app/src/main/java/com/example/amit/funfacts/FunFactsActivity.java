85a41c5f1dd7b61e39174a4b5b324a66a70bb100
Archana Kumar <archkum@gmail.com>
2014-09-10 22:31:52 -0700

    First Load
    First Load

---
+++ app/src/main/java/com/example/amit/funfacts/FunFactsActivity.java
@@ -0,0 +1,52 @@
+package com.example.amit.funfacts;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class FunFactsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        //Declare variables and give them values.
+
+        TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.funFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+        public void onClick (View view){
+
+            }
+
+        };
+        showFactButton.setOnClickListener(listener);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.fun_facts, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
