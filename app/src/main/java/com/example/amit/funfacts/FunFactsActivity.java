c749cd6f6d39f4b0b0bb218cc5ba9f0e04fd5501
Archana Kumar <archkum@gmail.com>
2014-09-24 22:44:39 -0700

    10 Random fun fact activities added alongwith random background colors.

--- app/src/main/java/com/example/amit/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/amit/funfacts/FunFactsActivity.java
@@ -1,16 +1,22 @@
 package com.example.amit.funfacts;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class FunFactsActivity extends Activity {
 
+    private FactBook mFactBook= new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +24,19 @@ public class FunFactsActivity extends Activity {
 
         //Declare variables and give them values.
 
-        TextView factLabel = (TextView) findViewById(R.id.factTextView);
-        Button showFactButton = (Button) findViewById(R.id.funFactButton);
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        final Button showFactButton = (Button) findViewById(R.id.funFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
-        public void onClick (View view){
+            public void onClick(View view) {
+                String fact = mFactBook.getFact();
 
+                //update the label with our dynamic fact
+                factLabel.setText(fact);
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
             }
 
         };
@@ -31,22 +44,4 @@ public class FunFactsActivity extends Activity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.fun_facts, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
